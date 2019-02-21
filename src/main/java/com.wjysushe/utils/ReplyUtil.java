a85902c03c6577af57a3bb8382546a353147088b
nangongyuan <1390811049@qq.com>
2019-02-21 18:44:22 +0800

    fix bug

--- src/main/java/com.wjysushe/utils/ReplyUtil.java
+++ src/main/java/com.wjysushe/utils/ReplyUtil.java
@@ -1,14 +1,14 @@
 /**
  * Copyright (C), 2015-2019, 虹软
- * FileName: UserService
+ * FileName: ReplyUtil
  * Author:   yuan
- * Date:     2019/2/21 9:02
+ * Date:     2019/2/21 18:40
  * Description:
  * History:
  * <author>          <time>          <version>          <desc>
  * yuan           修改时间           版本号              描述
  */
-package com.wjysushe.service;
+package com.wjysushe.utils;
 
 /**
  * 〈一句话功能简述〉<br> 
@@ -18,7 +18,11 @@ package com.wjysushe.service;
  * @create 2019/2/21
  * @since 1.0.0
  */
-public class UserService {
+public class ReplyUtil {
 
+    public static String alter(String msg){
+        String string = "";
 
+        return string;
+    }
 }
